7b7b10cec93b4a160abf5be52dcb24752c7491b2
JoeTomo95 <Joe.tomo95@hotmail.com>
2017-03-17 00:07:41 +0000

    Update map.js

--- Site/assets/js/map.js
+++ Site/assets/js/map.js
@@ -1,21 +1,194 @@
 $(document).ready(function() {
+	function initialize() {
+	  var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 13,
+		center: {lat: 51.453399, lng: -0.970917},
+		mapTypeId: google.maps.MapTypeId.HYBRID,
+		scrollwheel: true,
+		draggable: false,
+		panControl: true,
+		zoomControl: true,
+		mapTypeControl: true,
+		scaleControl: true,
+		streetViewControl: true,
+		overviewMapControl: true,
+		rotateControl: true,
+	  });
 
-    function initialize() {
-        var mapOptions = {
-            center: new google.maps.LatLng(28.1823294, -82.352912),
-            zoom: 9,
-            mapTypeId: google.maps.MapTypeId.HYBRID,
-            scrollwheel: false,
-            draggable: false,
-            panControl: true,
-            zoomControl: true,
-            mapTypeControl: true,
-            scaleControl: true,
-            streetViewControl: true,
-            overviewMapControl: true,
-            rotateControl: true,
-        };
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    }
+	  var image = 'assets/images/map-icon.png';
+	  var beachMarker = new google.maps.Marker({
+		position: {lat: 51.453399, lng: -0.970917},
+		map: map,
+		icon: image
+	  });
+	  var styledMapType = new google.maps.StyledMapType(
+	  [
+		  {
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#f5f5f5"
+			  }
+			]
+		  },
+		  {
+			"elementType": "labels.icon",
+			"stylers": [
+			  {
+				"visibility": "off"
+			  }
+			]
+		  },
+		  {
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#616161"
+			  }
+			]
+		  },
+		  {
+			"elementType": "labels.text.stroke",
+			"stylers": [
+			  {
+				"color": "#f5f5f5"
+			  }
+			]
+		  },
+		  {
+			"featureType": "administrative.land_parcel",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#bdbdbd"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#eeeeee"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#757575"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi.park",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#e5e5e5"
+			  }
+			]
+		  },
+		  {
+			"featureType": "poi.park",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#9e9e9e"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#ffffff"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.arterial",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#757575"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.highway",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#dadada"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.highway",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#616161"
+			  }
+			]
+		  },
+		  {
+			"featureType": "road.local",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#9e9e9e"
+			  }
+			]
+		  },
+		  {
+			"featureType": "transit.line",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#e5e5e5"
+			  }
+			]
+		  },
+		  {
+			"featureType": "transit.station",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#eeeeee"
+			  }
+			]
+		  },
+		  {
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+			  {
+				"color": "#c9c9c9"
+			  }
+			]
+		  },
+		  {
+			"featureType": "water",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			  {
+				"color": "#9e9e9e"
+			  }
+			]
+		  }
+		],
+		{name: 'Styled Map'});
+		
+		map.mapTypes.set('styled_map', styledMapType);
+		map.setMapTypeId('styled_map');
+	}
+    
     google.maps.event.addDomListener(window, 'load', initialize);
 });
+
+
